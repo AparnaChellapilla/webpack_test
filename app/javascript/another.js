6a361d2ab69a44f3819e656b794bf0d6ae457bee
AC <75492066+AparnaChellapilla@users.noreply.github.com>
2020-12-21 22:54:16 -0600

    first commit
    first step completion

--- app/javascript/another.js
+++ app/javascript/another.js
@@ -1,6 +1,6 @@
 function sayHello() {
     let header = document.querySelector('#greeting');
-    let hello = ('<h1> hello World </h1>');
+    let hello = ('<h1> Hello World </h1>');
     header.innerHTML = hello;
 }
 
